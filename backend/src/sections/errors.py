2cb80a3f5dcab550042ffd5ab0772338f41a3216
ark948 <armankarimi948@gmail.com>
2024-12-31 23:23:20 +0330

    Moved errors to their own module

--- backend/src/sections/errors.py
+++ backend/src/sections/errors.py
@@ -6,61 +6,18 @@ from sqlalchemy.exc import SQLAlchemyError
 
 
 
-class CustomException(Exception):
-    """ Base class for all errors """
-    pass
-
-
-class InvalidToken(CustomException):
-    """ User has provided an invalid or expired token """
-    pass
-
-
-
-class RevokedToken(CustomException):
-    """ User has provided a token that has been revoked """
-    pass
-
-
-
-class AccessTokenRequired(CustomException):
-    """ User has provided a refresh token when an access token is required """
-    pass
-
-
-
-class RefreshTokenRequired(CustomException):
-    """ User has provided an access token when a refresh token is required """
-    pass
-
-
-class UserAlreadyExists(CustomException):
-    """ User has provided an email for a user who already exists during sign up """
-    pass
-
-
-
-class InvalidCredentials(CustomException):
-    """ User has provided wrong email or password during sign up or log on """
-    pass
-
-
-
-class InsufficientPermission(CustomException):
-    """ User does not have neccessary permissions """
-    pass
-
-
-class UserNotFound(CustomException):
-    """" User not found """
-    pass
-
-
-
-class AccountNotVerified(CustomException):
-    """ User has not verified their account yet """
-    pass
-
+from src.sections.authentication.errors import (
+    CustomException,
+    UserAlreadyExists,
+    UserNotFound,
+    InvalidCredentials,
+    InvalidToken,
+    RevokedToken,
+    AccessTokenRequired,
+    AccountNotVerified,
+    RefreshTokenRequired,
+    InsufficientPermission
+)
 
 
 # Exception handler
